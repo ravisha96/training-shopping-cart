7aaf16b9e879b94fbb687241b2e06eda8368a941
Ravi sha <ravi.sha@nagarro.com>
2017-01-17 22:35:56 +0530

    read and write of button values
    1. click event added
    2. retrieving of values and displaying added.
    3. started with addition

--- calculator/script.js
+++ calculator/script.js
@@ -3,18 +3,56 @@
     /**
      * 1. On click on button we want to get the value
      *      a. Add click event to the buttons.
-     *      b. Get the value of the click button.
+     *      b. Get the value of the clicked button.
      * 
      * 
      * 2. Display the value in result section.
+     *      a. Add only values which are between 0-9
+     * 
+     * 3. Add operator based functionality.
+     *      a. Addition
+     *      b. Substraction
+     *      c. Division
+     *      d. Multiplication
+     *      e. Enter
      */
 
     // Added a click event listner to button element.
+    $(document).ready( Calculator );
 
-    $(document).ready( function () {
-        $('button').click(function () {
-            alert('Button is clicked');
+
+    function Calculator () {
+
+        $('button').click(function (event) {
+
+            // Get the user input value.
+            var userInputValue = event.target.value;
+
+
+            if(userInputValue >= 0 && userInputValue <= 9) {
+                // Get the value of result.
+                var getResultData = $('#result').val();
+
+                // Set the value of result.
+                $('#result').val(getResultData+userInputValue);
+            } else {
+
+                if(userInputValue === '+') {
+
+                    var getResultData = $('#result').val();
+                    
+                    addition(getResultData);
+
+                }
+
+            }
         });
-    });
+    }
+
+
+    function addition (value) {
+        console.log(value);
+    }
+
 
 })();
